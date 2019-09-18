6b651cf262b8feef0991d5b1c79deb93647b6576
Peter Maciocia <peter.maciocia@gmail.com>
2019-09-18 22:48:21 +0100

    Embed message for game

--- bot.py
+++ bot.py
@@ -16,8 +16,9 @@ async def on_ready():
 
 
 @bot.command(name='add_game', help='Add a game you will be bringing')
-async def add_game(ctx, game_name: str):
+async def add_game(ctx, *, message):
     user = ctx.message.author
+    game_name = message
 
     games = bgg.games(game_name)     
     if len(games) == 0:
@@ -41,8 +42,8 @@ async def add_game(ctx, game_name: str):
         embed.add_field(name="Best", value=recommend, inline=True)
 
         description=game.description
-        if len(game.description) > 512:
-            description = description[:509] + "..."
+        if len(game.description) > 300:
+            description = description[:297] + "..."
 
         embed.add_field(name="Description", value=description)
         embed.set_thumbnail(url=game.thumbnail)
@@ -51,7 +52,7 @@ async def add_game(ctx, game_name: str):
 
 @bot.command(name='games', help='List games that are being played')
 async def list_games(ctx):
-    response = f"Adding game {game_name}"
+    response = f"Adding game"
     await ctx.send(response)
 
 bot.run(token)
