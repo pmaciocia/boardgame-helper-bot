57a41624e05f63120bdddbc6ea48c76b2123eeb8
Peter Maciocia <peter.maciocia@gmail.com>
2020-06-07 16:01:14 +0100

    WIP

--- codenames.py
+++ codenames.py
@@ -38,7 +38,9 @@ class Codenames(commands.Cog):
     async def reset(self, ctx: commands.Context):
         logger.info(f"Resetting...")
         self.players.clear()
-        await game.delete()
+        await self.game.delete()
+
+        self.game = None
         await ctx.message.add_reaction('👌')
 
     @commands.command(name='_start_game', help='Start a game')
@@ -68,43 +70,52 @@ class Codenames(commands.Cog):
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
         logger.info(f"User {user.id} reacted with {reaction.emoji} for message {reaction.message.id}")
-
-        message = reaction.message
-
-        if user.bot:
-            logger.info(f"User {user.id} is a bot - ignoring")
-            return
-
+        
         game = self.game 
         if game is None:
             logger.info(f"No game in progress - ignoring")
+            return
+
+        if user.bot:
+            logger.info(f"User {user.id} is a bot - ignoring")
             return
 
         if reaction.emoji not in Team.values() and reaction.emoji != CAPT:
             logger.info(f"Unknown reaction {reaction.emoji} - removing")
             return
 
+        message = reaction.message
         logger.info(f"Game message is {self.game.id}")
         if message.id == self.game.id:
-            logger.info(f"Reactions are {message.reactions} ")
+            logger.info(f"Reactions are {message.reactions}")
 
-            player = Player(user)
-            if player not in self.players:
-                self.players[user] = player
-            
+            if user not in self.players and reaction.emoji == CAPT:
+                logger.info(f"User with no team trying to be captain")
+                return
+
+            player = self.players.setdefault(user, Player(user))
             await player.assign_from_reaction(reaction)
-            
-                    
 
     @commands.Cog.listener()
     async def on_reaction_remove(self, reaction, user):
         logger.info(f"User {user.id} removed reaction {reaction.emoji} for message {reaction.message.id}")
+        if user in self.players:
+            logger.info(f"Updating player {user.id}")
+            player = self.players[user]
+            if reaction.emoji == CAPT:
+                logger.info(f"Removing captain from player {user.id}")
+                player.is_captain = False
+                await player.update_nick()
+            else:
+                logger.info(f"Removing player {user.id}")
+                await player.remove_nick()
+                del self.players[user]
 
-        prefix = "{reaction.emoji} - "
+    @command.Cog.listener()
+    async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+        pass
 
-        if user.nick != None and user.nick.starts_with(prefix):
-            await user.edit(nick=user.nick[len(prefix):])
-        
+
 class Player:
     def __init__(self, user: discord.Member):
         self.user = user
@@ -113,18 +124,27 @@ class Player:
         self.team = None
 
     async def remove_nick(self):
-        if self.prefix:
-            await user.edit(nick=user.nick[len(self.prefix):])
+        if self.prefix and self.user.nick:
+            nick = self.user.nick[len(self.prefix):]
+            logger.info(f"Resetting user {self.user.id} nick to {nick}")
+            try:
+                await self.user.edit(nick=nick)
+            except:
+                logger.error(f"Failed to remove nick for user {self.user.id}")
             self.prefix = None
-
 
     async def update_nick(self):
         if self.prefix:
-            logger.info(f"Remove old prefix {self.prefix}")
             await self.remove_nick()
 
-        self.prefix = f"{CAPT if self.is_captain else ''}{self.team.value} - "
-        await self.user.edit(nick=f"{self.prefix}{self.user.nick or self.user.name}")
+        self.prefix = f"{CAPT if self.is_captain else ''} {self.team.value} - "
+        nick = f"{self.prefix}{self.user.nick or self.user.name}"
+
+        logger.info(f"Setting user {self.user.id} nick to {nick}")
+        try:
+            await self.user.edit(nick=nick)
+        except:
+            logger.error(f"Failed to set nick for user {self.user.id}")
 
     async def assign_from_reaction(self, reaction: discord.Reaction):
         emoji = reaction.emoji
@@ -138,17 +158,13 @@ class Player:
             if emoji in Team.values():
                 self.team = Team(emoji)
                 await self.update_nick()
-
         else:
             if emoji == CAPT:
-                self.is_captain = True
-                await self.update_nick()
-                
+                self.is_captain = True                
             else:
-                # Can't react with same emoji twice
                 r_team = Team(emoji)
-
                 self.is_captain = False
                 self.team = r_team
-                await self.update_nick()
+            await self.update_nick()
+       
 
